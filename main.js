46c216738ad4a8aa4b3e7dc8e2683f02aee00da1
ashachintujyothi143 <ashajyothi.v1997@gmail.com>
2018-04-08 15:03:46 +0530

    adeed copmlited

--- main.js
+++ main.js
@@ -11,10 +11,12 @@ function push(file,callback){
   xhr.send();
 }
 //calling function
-push("web.json",function(text){
+push("web.json",function(text) {
   let ind=JSON.parse(text);
   console.log(ind);
   profile(ind.basics);
+  skill(ind.skills);
+  edu(ind.education);
 } )
 //for getting through id
 var main=document.getElementById('main');
@@ -24,7 +26,7 @@ var main=document.getElementById('main');
 //creating a element
   var left=document.createElement("div");
   left.classList.add("left");
-  //left.setAttribute("id","left");
+  left.setAttribute("id","left");
   //appending to main
   main.appendChild(left);
     function profile(basic){
@@ -41,3 +43,37 @@ var main=document.getElementById('main');
       phone.textContent=basic.phone;
       left.appendChild(phone);
     }
+    var right=document.createElement("div");
+    right.classList.add("right");
+    right.textContent="Technical Skills:"
+    right.appendChild(document.createElement("HR"));
+    main.appendChild(right);
+    function skill(ntr){
+      var table=document.createElement("table");
+      var row="";
+      for(var i = 0; i <ntr.length; i++){
+        row +="<tr><td>"+ntr[i].name+"</td><td>"+ntr[i].value+"</td></tr>"
+      }
+      table.innerHTML=row;
+      right.appendChild(table);
+}
+function edu(Education){
+
+  var e1=document.createElement("div");
+  e1.classList.add("education");
+  e1.textContent="Education Details:";
+  e1.appendChild(document.createElement("HR"));
+  right.appendChild(e1);
+  for(i in Education){
+var e2=document.createElement("h2");
+e2.textContent=Education[i].qualification;
+  var ul=document.createElement("ul");
+  for(j in Education[i].college){
+    var list=document.createElement("li");
+    list.textContent=Education[i].college[j];
+    ul.appendChild(list);
+    e1.appendChild(e2);
+    e1.appendChild(ul);
+}
+  }
+}
